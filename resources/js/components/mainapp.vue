8cde361d58d93602cb7a1f9c46c75a2e001909a4
Saeed212223 <ghulamsaeed54@gmail.com>
2023-12-14 19:44:02 +0430

    employee deleted with all functionality

--- resources/js/components/mainapp.vue
+++ resources/js/components/mainapp.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <router-link to="/" class="btn btn-danger">First Page</router-link>
-    <router-link to="/secondpage" class="btn btn-danger">Second Page</router-link>
+    <router-link to="/secondpage" class="btn btn-danger" >Second Page</router-link>
     <router-view></router-view>
 
 </div>
